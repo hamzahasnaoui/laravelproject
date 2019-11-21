fd6fddc7bb9dfa63c89e74006fefe48240e5bea6
hamza <ici.hasnaouihamza@gmailcom>
2019-11-21 17:15:44 +0100

    create models

---
+++ grenade_blog/database/migrations/2019_11_21_141229_create_factures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFacturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factures', function (Blueprint $table) {
+            $table->bigIncrements('numFact');
+            $table->double('Prix_Unitaire');
+            $table->double('Prix_Total');
+            $table->integer('quantite');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factures');
+    }
+}
--- grenade_blog/database/migrations/2019_11_21_141245_create_fournisseurs_table.php
+++ grenade_blog/database/migrations/2019_11_21_141245_create_fournisseurs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateFournisseursTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('fournisseurs', function (Blueprint $table) {
+            $table->bigIncrements('numFrs');
+             $table->String('Nom');
+              $table->String('Email');
+               $table->bigInteger('Motdepasse');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +29,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('fournisseurs');
     }
 }
